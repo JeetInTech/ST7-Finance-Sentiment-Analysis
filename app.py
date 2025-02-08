a6b31dcce8518ae12be032db6a42d8024f3b1b1c
FIRST_NAME LAST_NAME <surajkaranakshay8@gmail.com>
2025-02-08 09:45:39 +0530

    to

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask,render_template
-app = Flask(__name__)
+from flask import Flask, render_template,request
+app=Flask(__name__)
+
 @app.route("/")
+
 def index():
     return render_template("index.html")
 
+@app.route("/predict",methods =["POST","get"])
+def predict():
+    if request.method=="POST":
+        msg=request.form.get("message")
+        print(msg)
+    else:
+        return render_template("predict.html")    
+    
 
-if __name__ == "__main__":
-    app.run()
-
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5151)
