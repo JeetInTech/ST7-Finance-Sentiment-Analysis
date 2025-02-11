fb6901b6d32eae129cff0645c24d8dea098375d6
FIRST_NAME LAST_NAME <surajkaranakshay8@gmail.com>
2025-02-11 11:32:24 +0530

    not sure

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask, render_template,request
+from flask import Flask,render_template,request
+from test import TextToNum
+import pickle
+
 app=Flask(__name__)
 
 @app.route("/")
-
 def index():
     return render_template("index.html")
 
-@app.route("/predict",methods =["POST","get"])
+@app.route("/predict",methods=['POST','GET'])
 def predict():
     if request.method=="POST":
         msg=request.form.get("message")
         print(msg)
-    else:
-        return render_template("predict.html")    
-    
+        ob=TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st=ob.stemme()
+        stem_vector=" ".join(st)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5151)
+        with open("vectorizer.pickle","rb") as vc:
+            vectorizer=pickle.load(vc)
+        vcdata=vectorizer.transform([stem_vector]).toarray()
+        print(vcdata)
+        
+        with open("model.pickle","rb") as mc:
+            model=pickle.load(mc)
+
+        pred=model.predict(vcdata)
+        print(pred)
+
+
+
+
+    else:
+        return render_template("predict.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port='5050')
